042aaff4ba4c5f2e7011b4252e4560c03516c400
Joe MacMahon <joe.macmahon@cern.ch>
2015-01-24 15:31:10 +0100

    Created a settings.js.

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
 var endpoints = require('./endpoints');
+var settings = require('./settings');
 
-var url = 'mongodb://192.168.178.28:27017/rico';
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(settings.mongo_url, function(err, db) {
     assert.equal(null, err);
-    console.log('Connected to server at %s.', url);
+    console.log('Connected to server at %s.', settings.mongo_url);
 
     var server = restify.createServer();
 
---
+++ server/settings.js
@@ -0,0 +1,1 @@
+module.exports.mongo_url = 'mongodb://0.0.0.0:27017/rico'
