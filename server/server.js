576ed3f31f9831bc0c131d6709c1b86ef0bb4802
Joe MacMahon <joe.macmahon@cern.ch>
2015-01-24 16:32:15 +0100

    Added licensing information.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,22 @@
+/*
+Copyright 2015 Adrian Baran, Joe MacMahon, Leonardo Rossi
+
+This file is part of Rico.
+
+Rico is free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Rico is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with Rico.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 var restify = require('restify');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
