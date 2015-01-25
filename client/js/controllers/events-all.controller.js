89d207368d7186ffff78910ef98aca2cea430794
Leonardo Rossi <leonardo.r@cern.ch>
2015-01-25 13:21:44 +0100

    client: modularize controller and services
    Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

---
+++ client/js/controllers/events-all.controller.js
@@ -0,0 +1,61 @@
+
+/*
+Copyright 2015 Adrian Baran, Joe MacMahon, Leonardo Rossi
+
+This file is part of Rico.
+
+Rico is free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Rico is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with Rico.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Controller for All Events View
+
+RicoApp.controller('EventsAllCtrl', [ '$scope', '$routeParams', '$route', '$rootScope', 'EventsRestAPI', '$modal',
+		function($scope, $routeParams, $route, $rootScope, EventsRestAPI, $modal) {
+			$scope.bid = $routeParams.bid;
+			// $scope.board = BoardService.get($scope.bid);
+      EventsRestAPI.query(function(data){
+        events = data
+
+        var myDays= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+        events.forEach(function(event) {
+          var s_date = new Date(event['start_time'])
+          var sd = myDays[s_date.getDay()]
+          var sh = s_date.getHours()
+          var sm = s_date.getMinutes()
+
+          event['start_time'] = sd + " " + sh + ":" + sm
+
+          var e_date = new Date(event['end_time'])
+          var ed = myDays[e_date.getDay()]
+          var eh = e_date.getHours()
+          var em = e_date.getMinutes()
+
+          event['end_time'] = ed + " " + eh + ":" + em
+        });
+
+        $scope.events = events
+
+      })
+
+      $scope.open = function(event){
+        $modal.open({
+          title: event.title,
+          template: event.description,
+          size: 'lg',
+        })
+      };
+		}
+]);
+
--- client/js/main_ctrl.js
+++ client/js/main_ctrl.js
@@ -1,21 +1,21 @@
-/**
- * Copyright (C) 2014 Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
- *
- * This source code is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This source code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this source code; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- */
+/*
+Copyright 2015 Adrian Baran, Joe MacMahon, Leonardo Rossi
+
+This file is part of Rico.
+
+Rico is free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Rico is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with Rico.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 var RicoApp = angular.module("RicoApp", [ 'ngRoute', 'ngResource', 'ui.bootstrap' ]);
 
@@ -61,45 +61,6 @@ RicoApp.controller('MainCtrl', [
 		}
 ]);
 
-RicoApp.controller('EventsAllCtrl', [ '$scope', '$routeParams', '$route', '$rootScope', 'EventsRestAPI', '$modal',
-		function($scope, $routeParams, $route, $rootScope, EventsRestAPI, $modal) {
-			$scope.bid = $routeParams.bid;
-			// $scope.board = BoardService.get($scope.bid);
-      EventsRestAPI.query(function(data){
-        events = data
-
-        var myDays= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-
-        events.forEach(function(event) {
-          var s_date = new Date(event['start_time'])
-          var sd = myDays[s_date.getDay()]
-          var sh = s_date.getHours()
-          var sm = s_date.getMinutes()
-
-          event['start_time'] = sd + " " + sh + ":" + sm
-
-          var e_date = new Date(event['end_time'])
-          var ed = myDays[e_date.getDay()]
-          var eh = e_date.getHours()
-          var em = e_date.getMinutes()
-
-          event['end_time'] = ed + " " + eh + ":" + em
-        });
-
-        $scope.events = events
-
-      })
-
-      $scope.open = function(event){
-        $modal.open({
-          title: event.title,
-          template: event.description,
-          size: 'lg',
-        })
-      };
-		}
-]);
-
 // TODO convert in "save in Favourites"
 RicoApp.controller('eventChooseCtrl', [
 		'$scope',
@@ -140,11 +101,4 @@ RicoApp.controller('eventDeleteCtrl', [ '$scope', '$routeParams', '$http',
 
 		} ]);
 
-RicoApp.factory("EventsRestAPI", ['$resource', function($resource) {
-  return $resource('http://192.168.178.30:8080/events');
-}]);
-
-RicoApp.factory("SessionsRestAPI", ['$resource', function($resource) {
-  return $resource('http://192.168.178.30:8080/sessions/:uid/:sid');
-}]);
 
